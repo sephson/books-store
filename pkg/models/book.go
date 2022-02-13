366536d3075f767e25c1dd7c74306a94ec3aaed6
sephson <disutj@gmail.com>
2022-02-13 07:52:57 +0100

    worked on model functions

--- pkg/models/book.go
+++ pkg/models/book.go
@@ -18,4 +18,29 @@ type Book struct{
 func init(){
 	config.Connect()
 	db = config.GetDB()
+	db.AutoMigrate(&Book{})
+}
+
+func (b *Book) CreateBook() *Book{
+	db.NewRecord(db)
+	db.Create(&b)
+	return b
+}
+
+func GetAllBooks() []Book{
+	var Books []Book
+	db.Find(&Books)
+	return Books
+}
+
+func GetBookById(Id, int64) {*Book, *gotm.DB}{
+	var getBook Book
+	db:=db.where("ID=7", ID).Find(&getBook)
+	return &getBook, db
+}
+
+func DeleteBook(ID int64) Book{
+	var book Book
+	db.where("ID=7", ID).Delete(book)
+	return book
 }
